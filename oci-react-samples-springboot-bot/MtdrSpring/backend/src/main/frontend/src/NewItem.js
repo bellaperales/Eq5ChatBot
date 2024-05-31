cd908dc210fa34b710c5865cab88912bc1dd8ba4
a01177702 <132400887+a01177702@users.noreply.github.com>
2024-05-31 16:02:51 -0600

    Revert "Container sides, col"
    This reverts commit f2ad36930cf627e4beace1ed4d0aa7818de4d2b9.

--- oci-react-samples-springboot-bot/MtdrSpring/backend/src/main/frontend/src/NewItem.js
+++ oci-react-samples-springboot-bot/MtdrSpring/backend/src/main/frontend/src/NewItem.js
@@ -1,6 +1,17 @@
-
+/*
+## MyToDoReact version 1.0.
+##
+## Copyright (c) 2022 Oracle, Inc.
+## Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+*/
+/*
+ * Component that supports creating a new todo item.
+ * @author  jean.de.lavarene@oracle.com
+ */
 
 import React, { useState } from "react";
+import Button from '@mui/material/Button';
+
 
 function NewItem(props) {
   const [item, setItem] = useState('');
@@ -19,31 +30,33 @@ function NewItem(props) {
   }
   return (
     <div id="newinputform">
-      <form>
-        <input
-          id="newiteminput"
-          placeholder="New item"
-          type="text"
-          autoComplete="off"
-          value={item}
-          onChange={handleChange}
-          // No need to click on the "ADD" button to add a todo item. You
-          // can simply press "Enter":
-          onKeyDown={event => {
-            if (event.key === 'Enter') {
-              handleSubmit(event);
-            }
-          }}
-        />
-        <span>&nbsp;&nbsp;</span>
-        <button
-          className="AddButton"
-          disabled={props.isInserting}
-          onClick={!props.isInserting ? handleSubmit : null}
-        >
-          {props.isInserting ? 'Adding…' : 'Add'}
-        </button>
-      </form>
+    <form>
+      <input
+        id="newiteminput"
+        placeholder="New item"
+        type="text"
+        autoComplete="off"
+        value={item}
+        onChange={handleChange}
+        // No need to click on the "ADD" button to add a todo item. You
+        // can simply press "Enter":
+        onKeyDown={event => {
+          if (event.key === 'Enter') {
+            handleSubmit(event);
+          }
+        }}
+      />
+      <span>&nbsp;&nbsp;</span>
+      <Button
+        className="AddButton"
+        variant="contained"
+        disabled={props.isInserting}
+        onClick={!props.isInserting ? handleSubmit : null}
+        size="small"
+      >
+        {props.isInserting ? 'Adding…' : 'Add'}
+      </Button>
+    </form>
     </div>
   );
 }
