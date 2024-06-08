34e3a115b3368c8b98dfd09e15c961370061b47e
Marcelo Garcia <105244132+MGPV@users.noreply.github.com>
2024-06-07 20:49:07 -0600

    Update build.sh
    docker login agregado

--- oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
+++ oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
@@ -5,6 +5,12 @@ export IMAGE_VERSION=0.1
 
 export JAVA_HOME="/usr/lib64/graalvm/graalvm22-ee-java17"
 
+61de9d3c35ac
+
+echo "Logging in to Docker registry"
+docker login -u "61de9d3c35ac/a00815371@tec.mx" --password-stdin "mx-queretaro-1.ocir.io"
+
+
 # Set DOCKER_REGISTRY directly
 export DOCKER_REGISTRY="mx-queretaro-1.ocir.io/axvfutv1sy8e/eq5chatbot/wu5ru"
 echo "DOCKER_REGISTRY (build.sh) set to: $DOCKER_REGISTRY"
