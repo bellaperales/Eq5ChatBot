5dfb9667ee0c0462a6e54cdc1d9896074d52e899
Marcelo Garcia <105244132+MGPV@users.noreply.github.com>
2024-06-07 19:51:37 -0600

    Update build.sh
    docker_registry fijo

--- oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
+++ oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
@@ -4,15 +4,9 @@ export IMAGE_NAME=todolistapp-springboot
 export IMAGE_VERSION=0.1
 
 
-if [ -z "$DOCKER_REGISTRY" ]; then
-    echo "DOCKER_REGISTRY not set. Will get it with state_get"
-  export DOCKER_REGISTRY=$(state_get DOCKER_REGISTRY)
-fi
-
-if [ -z "$DOCKER_REGISTRY" ]; then
-    echo "Error: DOCKER_REGISTRY env variable needs to be set!"
-    exit 1
-fi
+# Set DOCKER_REGISTRY directly
+export DOCKER_REGISTRY="mx-queretaro-1.ocir.io/axvfutv1sy8e/eq5chatbot/wu5ru"
+echo "DOCKER_REGISTRY (build.sh) set to: $DOCKER_REGISTRY"
 export IMAGE=${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
 
 mvn clean package spring-boot:repackage
