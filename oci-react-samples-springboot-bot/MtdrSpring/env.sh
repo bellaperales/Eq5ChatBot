2f0e80d04d8d9335dd445c6e44793b7bd6d414ae
Marcelo Garcia <105244132+MGPV@users.noreply.github.com>
2024-06-07 18:52:07 -0600

    Update env.sh
    logging para source env.sh

--- oci-react-samples-springboot-bot/MtdrSpring/env.sh
+++ oci-react-samples-springboot-bot/MtdrSpring/env.sh
@@ -2,11 +2,15 @@
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+echo "Starting env.sh..."
+
 # Make sure this is run via source or .
 if ! (return 0 2>/dev/null); then
   echo "ERROR: Usage 'source env.sh'"
   exit
 fi
+
+echo "Passed sourcing check..."
 
 # POSIX compliant find and replace
 function sed_i(){
