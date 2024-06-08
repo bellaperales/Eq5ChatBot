e3e35900959f39b46ec7c7dedda20cf0e16436b8
Marcelo Garcia <105244132+MGPV@users.noreply.github.com>
2024-06-08 17:30:10 -0600

    Update build.sh
    moví el login de lugar

--- oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
+++ oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
@@ -6,17 +6,16 @@ export IMAGE_VERSION=0.1
 export JAVA_HOME="/usr/lib64/graalvm/graalvm22-ee-java17"
 
 
-#echo "Logging in to Docker registry"
-echo "3VMLLvkIjJQn(bUXSMfQ" | docker login -u "axvfutv1sy8e/a00815371@tec.mx" --password-stdin "mx-queretaro-1.ocir.io"
-
-
 # Set DOCKER_REGISTRY directly
 export DOCKER_REGISTRY="mx-queretaro-1.ocir.io/axvfutv1sy8e/eq5chatbot/wu5ru"
 echo "DOCKER_REGISTRY (build.sh) set to: $DOCKER_REGISTRY"
 export IMAGE=${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
 
 mvn clean package spring-boot:repackage
-docker build -f Dockerfile -t $IMAGE .
+docker build -f Dockerfile -t $IMAGE --platform=linux/arm64/v8.
+
+#echo "Logging in to Docker registry"
+echo "3VMLLvkIjJQn(bUXSMfQ" | docker login -u "axvfutv1sy8e/a00815371@tec.mx" --password-stdin "mx-queretaro-1.ocir.io"
 
 docker push $IMAGE
 if [  $? -eq 0 ]; then
