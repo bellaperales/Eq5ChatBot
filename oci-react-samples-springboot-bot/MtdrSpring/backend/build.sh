2a3809770ffadfd3e0ae4da8e97d35221f773409
Marcelo Garcia <105244132+MGPV@users.noreply.github.com>
2024-06-12 23:34:43 -0600

    Update build.sh
    echos

--- oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
+++ oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
@@ -15,6 +15,11 @@ if [ -z "$DOCKER_REGISTRY" ]; then
 fi
 export IMAGE=${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
 
+# Debug prints
+echo "DOCKER_REGISTRY: $DOCKER_REGISTRY"
+echo "IMAGE: $IMAGE"
+
+
 echo "which mvn: $(which mvn)"
 echo "pwd: $(pwd)"
 echo "JAVA: $JAVA_HOME"
@@ -28,6 +33,9 @@ docker build -f Dockerfile -t $IMAGE --platform=linux/arm64/v8 .
 echo "3VMLLvkIjJQn(bUXSMfQ" | docker login -u "axvfutv1sy8e/a00815371@tec.mx" --password-stdin "mx-queretaro-1.ocir.io"
 
 docker push $IMAGE
-if [  $? -eq 0 ]; then
+if [ $? -eq 0 ]; then
+    echo "Docker push successful, removing local image..."
     docker rmi "$IMAGE"
+else
+    echo "Docker push failed, not removing local image."
 fi
