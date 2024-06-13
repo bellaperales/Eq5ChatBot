916ff2aff660eb09b1164823e759466b34972019
Marcelo Garcia <105244132+MGPV@users.noreply.github.com>
2024-06-12 19:38:43 -0600

    Update build.sh
    reset build

--- oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
+++ oci-react-samples-springboot-bot/MtdrSpring/backend/build.sh
@@ -3,18 +3,28 @@
 export IMAGE_NAME=todolistapp-springboot
 export IMAGE_VERSION=0.1
 
-export JAVA_HOME="/usr/lib64/graalvm/graalvm22-ee-java17"
 
+if [ -z "$DOCKER_REGISTRY" ]; then
+    echo "DOCKER_REGISTRY not set. Will get it with state_get"
+  export DOCKER_REGISTRY=$(state_get DOCKER_REGISTRY)
+fi
 
-# Set DOCKER_REGISTRY directly
-export DOCKER_REGISTRY="mx-queretaro-1.ocir.io/axvfutv1sy8e/eq5chatbot/wu5ru"
-echo "DOCKER_REGISTRY (build.sh) set to: $DOCKER_REGISTRY"
+if [ -z "$DOCKER_REGISTRY" ]; then
+    echo "Error: DOCKER_REGISTRY env variable needs to be set!"
+    exit 1
+fi
 export IMAGE=${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
 
-mvn clean package spring-boot:repackage
-docker build -f Dockerfile -t $IMAGE --platform=linux/arm64/v8 .
+echo "which mvn: $(which mvn)"
+echo "pwd: $(pwd)"
+echo "JAVA: $JAVA_HOME"
+# echo "mvn: $(mvn package)"
+echo "mvn: $(mvn clean package spring-boot:repackage)"
+echo "jarfile: $(ls -la target)"
 
-#echo "Logging in to Docker registry"
+
+docker build -f Dockerfile -t $IMAGE --platform=linux/arm64/v8 .
+#docker login
 echo "3VMLLvkIjJQn(bUXSMfQ" | docker login -u "axvfutv1sy8e/a00815371@tec.mx" --password-stdin "mx-queretaro-1.ocir.io"
 
 docker push $IMAGE
