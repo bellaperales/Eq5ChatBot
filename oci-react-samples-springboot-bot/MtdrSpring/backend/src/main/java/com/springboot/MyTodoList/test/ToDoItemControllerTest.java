007603ad4417313eca175973e4f5c76bbcb8d37a
AGG_11387_4 <A01138704@tec.mx>
2024-06-14 00:12:34 -0600

    Update test file inside java and lint update

--- oci-react-samples-springboot-bot/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/test/ToDoItemControllerTest.java
+++ oci-react-samples-springboot-bot/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/test/ToDoItemControllerTest.java
@@ -12,8 +12,8 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import com.springboot.MyTodoList.controller.ToDoItemBotController;
 import com.springboot.MyTodoList.service.EmployeeItemService;
@@ -26,33 +26,21 @@ import com.springboot.MyTodoList.util.BotCommands;
  */
 class ToDoItemBotControllerTest {
 
-    /**
-     * Chat ID for testing.
-     */
     private static final long CHAT_ID = 123456789L;
-
-    /**
-     * User ID for testing.
-     */
     private static final int USER_ID = 12345;
 
-    /**
-     * Tests start command processing.
-     */
     @Test
     void shouldProcessStartCommand() throws TelegramApiException {
-        ToDoItemService toDoItemServiceMock = mock(ToDoItemService.class);
-        ProjectItemService projectItemServiceMock = mock(ProjectItemService.class);
-        EmployeeItemService employeeItemServiceMock = mock(EmployeeItemService.class);
+        ToDoItemService toDoService = mock(ToDoItemService.class);
+        ProjectItemService projectService = mock(ProjectItemService.class);
+        EmployeeItemService employeeService = mock(EmployeeItemService.class);
 
         ToDoItemBotController bot = new ToDoItemBotController(
-            "7198383080:AAF1YvgOUUDbv7fmclRmEFn3DXGaoJGA3rM",
-            "a00815371_bot", toDoItemServiceMock,
-            projectItemServiceMock, employeeItemServiceMock);
+            "7198383080:AAF-YvgOUUDbv7fmclRmEFn3DXGaoJGA3rM",
+            "a00815371_bot", toDoService, projectService, employeeService);
 
-        TelegramLongPollingBot telegramBotMock = mock(TelegramLongPollingBot.class);
-        when(telegramBotMock.execute(any(SendMessage.class)))
-            .thenReturn(new Message());
+        TelegramLongPollingBot telegramBot = mock(TelegramLongPollingBot.class);
+        when(telegramBot.execute(any(SendMessage.class))).thenReturn(new Message());
 
         User user = new User();
         user.setId((long) USER_ID);
@@ -70,43 +58,6 @@ class ToDoItemBotControllerTest {
 
         bot.onUpdateReceived(testUpdate);
 
-        verify(telegramBotMock, times(1)).execute(any(SendMessage.class));
-    }
-
-    /**
-     * Tests todo list command processing for a developer.
-     */
-    @Test
-    void shouldProcessTodoListCommandForDeveloper() throws TelegramApiException {
-        ToDoItemService toDoItemServiceMock = mock(ToDoItemService.class);
-        ProjectItemService projectItemServiceMock = mock(ProjectItemService.class);
-        EmployeeItemService employeeItemServiceMock = mock(EmployeeItemService.class);
-
-        ToDoItemBotController bot = new ToDoItemBotController(
-            "7198383080:AAF1YvgOUUDbv7fmclRmEFn3DXGaoJGA3rM",
-            "a00815371_bot", toDoItemServiceMock,
-            projectItemServiceMock, employeeItemServiceMock);
-
-        TelegramLongPollingBot telegramBotMock = mock(TelegramLongPollingBot.class);
-        when(telegramBotMock.execute(any(SendMessage.class)))
-            .thenReturn(new Message());
-
-        User user = new User();
-        user.setId((long) USER_ID);
-
-        Chat chat = new Chat();
-        chat.setId(CHAT_ID);
-        chat.setType("private");
-
-        Message testMessage = new Message();
-        testMessage.setChat(chat);
-        testMessage.setText("/todolist");
-
-        Update testUpdate = new Update();
-        testUpdate.setMessage(testMessage);
-
-        bot.onUpdateReceived(testUpdate);
-
-        verify(telegramBotMock, times(1)).execute(any(SendMessage.class));
+        verify(telegramBot, times(1)).execute(any(SendMessage.class));
     }
 }
---
+++ package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains tests for the MyTodoList application.
+ */
+package com.springboot.MyTodoList.test;
+
